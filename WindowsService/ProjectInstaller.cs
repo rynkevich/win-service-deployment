eb25d822b930133c462369903a1f90fdfe179e8b
Arseni Rynkevich <rynkevich@icloud.com>
2020-01-22 12:25:02 -0800

    Add demo Windows Service

---
+++ WindowsService/ProjectInstaller.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace WindowsService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+        private void ServiceInstaller_AfterInstall(object sender, InstallEventArgs e)
+        {
+            using (var serviceController = new ServiceController(ServiceInstaller.ServiceName))
+            {
+                serviceController.Start();
+            }
+        }
+    }
+}
---
+++ WindowsService/Service.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+
+namespace WindowsService
+{
+    public partial class Service : ServiceBase
+    {
+        private const string LogPath = "C:\\ServiceLog.txt";
+
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            LogToFile("Service started.");
+        }
+
+        protected override void OnStop()
+        {
+            LogToFile("Service stopped.");
+        }
+
+        private static void LogToFile(string text)
+        {
+            using (var writer = new StreamWriter(LogPath, append: true))
+            {
+                writer.WriteLine($"{DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")}: {text}");
+            }
+        }
+    }
+}
